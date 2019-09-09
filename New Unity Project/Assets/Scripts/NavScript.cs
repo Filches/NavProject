1e3a752c98e67261d1faa4d95a9a5d0b5098cd0d
Ariel <kailichen99@gmail.com>
2019-09-10 11:52:59 +1200

    cat sliding

---
+++ New Unity Project/Assets/Scripts/NavScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavScript : MonoBehaviour
+{ 
+    public List<Transform> destination;
+    private NavMeshAgent cat;
+    private int currentDestination;
+    public float distanceThreshold = 1;
+    public float gap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cat = GetComponent<NavMeshAgent>();
+        currentDestination = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cat.remainingDistance < distanceThreshold)
+        {
+            NextDestination();
+        }
+
+        cat.SetDestination(destination[currentDestination].position);
+
+        gap = cat.remainingDistance;
+    }
+
+    public void NextDestination()
+    {
+        currentDestination++;
+
+        if (currentDestination > destination.Count - 1)
+        {
+            currentDestination = 0;
+        }
+
+        cat.SetDestination(destination[currentDestination].position);
+    }
+}
