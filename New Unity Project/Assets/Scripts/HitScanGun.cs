09860bf6ba26531c34741f7817ac8e17792f8623
Ariel <kailichen99@gmail.com>
2019-09-11 11:47:12 +1200

    pew

---
+++ New Unity Project/Assets/Scripts/HitScanGun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScanGun : MonoBehaviour
+{
+    public Transform gunBarrel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int layermask = 1 << 8;
+        layermask = ~layermask;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(gunBarrel.position, gunBarrel.forward, out hit, Mathf.Infinity, layermask))
+        {
+            Debug.DrawRay(gunBarrel.position, gunBarrel.forward * hit.distance, Color.green);
+        }
+        else
+        {
+            Debug.DrawRay(gunBarrel.position, gunBarrel.forward * 9000, Color.red);
+        }
+    }
+}
